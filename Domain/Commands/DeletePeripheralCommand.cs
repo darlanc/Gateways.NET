9d58474925860455cc1c33241c45247e8bf2c17f
Juan David Berovides <juandbm@gmail.com>
2022-01-20 22:30:26 -0500

    Day 2: Domain commands

---
+++ Domain/Commands/DeletePeripheralCommand.cs
@@ -0,0 +1,15 @@
+﻿using Gateways.NET.Contracts;
+
+namespace Gateways.NET.Domain.Commands
+{
+    /// <summary>
+    /// Command for delete a Peripheral device
+    /// </summary>
+    public class DeletePeripheralCommand : ICommand
+    {
+        /// <summary>
+        /// Peripheral device ID
+        /// </summary>
+        public int PeripheralId { get; set; }
+    }
+}
