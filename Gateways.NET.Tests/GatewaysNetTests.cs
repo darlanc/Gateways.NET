32524c6cb282eaae04cc1df2027ea260eb13d3af
Juan David Berovides <juandbm@gmail.com>
2022-01-26 22:05:40 -0500

    Completing Gateways Unit Tests

--- Gateways.NET.Tests/GatewaysNetTests.cs
+++ Gateways.NET.Tests/GatewaysNetTests.cs
@@ -16,10 +16,13 @@ namespace Gateways.NET.Tests
 
         #region [ Support Methods ]
 
+        protected int innerCount = 0;
+
         protected string GetSerialNumber(int consecutive = 0)
         {
             var date = DateTime.Now;
-            return $"{date.Year}{date.Month}{date.Day}-{date.Hour}{date.Minute}{date.Second}-{consecutive:n4}";
+            innerCount++;
+            return $"{date.Year}{date.Month}{date.Day}-{date.Hour}{date.Minute}{date.Second}-{(innerCount + consecutive):n4}";
         }
 
         protected string GetRandomIpv4()
@@ -28,12 +31,12 @@ namespace Gateways.NET.Tests
             return $"{rand.Next(0, 256)}.{rand.Next(0, 256)}.{rand.Next(0, 256)}.{rand.Next(0, 256)}";
         }
 
-        protected uint GetUID()
+        protected uint GetUID(uint consecutive = 0)
         {
             var date = DateTime.Now;
             var startDate = new DateTime(2022, 1, 26);
             var ticks = date.Ticks - startDate.Ticks;
-            return (uint)ticks;
+            return (uint)ticks + consecutive;
         }
 
         public void AssertAreEquals(PeripheralViewModel source, PeripheralViewModel target)
@@ -122,7 +125,7 @@ namespace Gateways.NET.Tests
         {
             var model = new GatewayViewModel
             {
-                SerialNumber = GetSerialNumber(),
+                SerialNumber = GetSerialNumber(1),
                 IpAddress = GetRandomIpv4(),
                 Name = "Test update Gateway"
             };
@@ -164,6 +167,56 @@ namespace Gateways.NET.Tests
             var parentGateway = await sdk.Gateways.GetGateaway(id);
             Assert.IsTrue(parentGateway.Peripherals.Length == 1);
             AssertAreEquals(parentGateway.Peripherals[0], peripheralModel);
+        }
+
+        #endregion
+
+        #region [ Peripherals Max Limit ]
+
+        [TestMethod]
+        public async Task CheckPeripheralsLimit()
+        {
+            var model = new GatewayViewModel
+            {
+                SerialNumber = GetSerialNumber(),
+                IpAddress = GetRandomIpv4(),
+                Name = "Test full Gateway"
+            };
+            var gateway = await sdk.Gateways.AddGateway(model);
+            var id = gateway.Id;
+
+            //Add 10 peripherals (max)
+            for (uint i = 0; i < 10; i++)
+            {
+                var peripheralModel = new PeripheralViewModel
+                {
+                    UID = GetUID(i+1),
+                    Vendor = "Musala Soft",
+                    Status = true
+                };
+                await sdk.Gateways.AddPeripheral(id, peripheralModel);
+            }
+
+            //Check peripherals list of the Gateway
+            var parentGateway = await sdk.Gateways.GetGateaway(id);
+            Assert.IsTrue(parentGateway.Peripherals.Length == 10);
+
+            try
+            {
+                // Attempt to add a new peripheral over the limit
+                var peripheralModel = new PeripheralViewModel
+                {
+                    UID = GetUID(11),
+                    Vendor = "Musala Soft",
+                    Status = true
+                };
+                await sdk.Gateways.AddPeripheral(id, peripheralModel);
+                throw new Exception("Test failed, the new peripheral was added over the limit");
+            }
+            catch (ApiException ex)
+            {
+                Assert.IsTrue(ex.Code == (int)HttpStatusCode.BadRequest);
+            }
         }
 
         #endregion
