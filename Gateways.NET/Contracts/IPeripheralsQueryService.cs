24d7d006d9e03ae3da51de77c074735baa4baa22
Juan David Berovides <juandbm@gmail.com>
2022-01-29 00:36:15 -0500

    Get Peripheral(s) endpoints

--- Gateways.NET/Contracts/IPeripheralsQueryService.cs
+++ Gateways.NET/Contracts/IPeripheralsQueryService.cs
@@ -1,8 +1,11 @@
 ﻿using Gateways.NET.Models;
+using System.Threading.Tasks;
 
 namespace Gateways.NET.Contracts
 {
     public interface IPeripheralsQueryService : IQueryService<Peripheral>
     {
+        Task<bool> Exists(int id);
+        Task<Peripheral> FindById(int id);
     }
 }
